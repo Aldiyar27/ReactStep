e9aba5c88ca72dab20dd64b4f95ebf996c8d9c2e
Ашкеев Алдияр Русланович <@lcoast.itstep>
2023-08-23 17:43:11 +0600

    lesson_1 - commit_4

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -5,6 +5,7 @@ import About from './pages/about';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Book from './pages/book';
 import Music from './pages/music';
+import Cook from './pages/cook';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/book" element={<Book/>}/>
         <Route path="/music" element={<Music/>}/>
+        <Route path="/cook" element={<Cook/>}/>
       </Routes>
     </Router>
   );
