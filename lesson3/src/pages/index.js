0ce2a2989048abdf1bc827330f2e4fc5af517b2d
Ашкеев Алдияр Русланович <@lcoast.itstep>
2023-09-06 17:34:02 +0600

    lesson_3 - commit_2

--- lesson3/src/pages/index.js
+++ lesson3/src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import "../css/counter.css";
 
 const Main = () => {
     const [count, setCount] = useState(0)
+    const [activeIndex, setActiveIndex] = useState(0);
 
     function clickMe(amount, sign){
         switch(sign){
@@ -18,16 +18,39 @@ const Main = () => {
 
     function Button(props){
         return (
-            <button className="blue" onClick={() => clickMe(props.amount, props.sign)}>
-                {props.sign}{props.amount}
+            <button className="button" onClick={() => clickMe(props.amount, props.sign)}>
+                <div class="rotate-block">
+                    <div class="change-img">
+                        <div class="front-side-img"><img src="сoin.png" alt=""/></div>
+                        <div class="back-side-img"><img src="coin.png" alt=""/></div>
+                    </div>
+                </div>
             </button>
         )
     }
 
+    function openNav() {
+        document.getElementById("mySidepanel").style.height = "600px";
+        document.querySelector(".closebtn").style.display = 'block';
+    }
+      
+      /* Установите ширину боковой панели в 0 (скройте ее) */
+      function closeNav() {
+        document.getElementById("mySidepanel").style.height = "0";
+        document.querySelector(".closebtn").style.display = 'none';
+    }
+      
     return ( 
         <div className="box">
             {count}
             <Button amount={1} sign={'+'} />
+            <div id="mySidepanel" class="sidepanel">
+                <a class="closebtn" onClick={() => closeNav()}>&times;</a>
+                <button className='button-menu'></button>
+                <button className='button-menu'></button>
+                <button className='button-menu'></button>
+            </div>
+            <button class="openbtn" onClick={() => openNav()}> <div className='text'>&#9776; Магазин </div></button>
         </div> 
     )
 }
