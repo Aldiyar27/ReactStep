85ec8c7bf81fef189b35246a41d5acd42a1ae513
Ашкеев Алдияр Русланович <@lcoast.itstep>
2023-09-08 16:14:25 +0600

    lesson_3 - commit_3

--- lesson3/src/pages/index.js
+++ lesson3/src/pages/index.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useLinkClickHandler } from 'react-router-dom';
 import "../css/counter.css";
 
 const Main = () => {
     const [count, setCount] = useState(0)
     const [activeIndex, setActiveIndex] = useState(0);
+    const [num, setNum] = useState(1)
 
-    function clickMe(amount, sign){
-        switch(sign){
-            case "+":
-                setCount(count + amount)
-                break
-            case "-":
-                setCount(count - amount)
-                break
-        }
+    function clickMe(amount){
+        setCount(count + amount)
     }
 
     function Button(props){
@@ -40,15 +35,52 @@ const Main = () => {
         document.querySelector(".closebtn").style.display = 'none';
     }
       
+    function Clicker(tab){
+        switch(tab){
+            case 1:
+                if(count >= 20){
+                    setCount(count - 20)
+                    setNum(10)
+                }
+                break
+            case 2:
+                if(count >= 100){
+                    setCount(count - 100)
+                    setNum(25)
+                }
+                break
+            case 3:
+                if(count >= 700){
+                    setCount(count - 700)
+                    setNum(35)
+                }
+                break
+            case 4:
+                if(count >= 1500){
+                    setCount(count - 1500)
+                    setNum(50)
+                }
+                break
+            case 5:
+                if(count >= 2500){
+                    setCount(count - 2500)
+                    setNum(70)
+                }
+                break
+        }
+    }
+
     return ( 
         <div className="box">
             {count}
-            <Button amount={1} sign={'+'} />
+            <Button amount={num}/>
             <div id="mySidepanel" class="sidepanel">
                 <a class="closebtn" onClick={() => closeNav()}>&times;</a>
-                <button className='button-menu'></button>
-                <button className='button-menu'></button>
-                <button className='button-menu'></button>
+                <button className='button-menu' onClick={() => Clicker(1)}>Clicker - 20$</button>
+                <button className='button-menu' onClick={() => Clicker(2)}>Phone - 100$</button>
+                <button className='button-menu' onClick={() => Clicker(3)}>Old Computer - 700$</button>
+                <button className='button-menu' onClick={() => Clicker(4)}>Laptop - 1500$</button>
+                <button className='button-menu' onClick={() => Clicker(5)}>Server - 2500$</button>
             </div>
             <button class="openbtn" onClick={() => openNav()}> <div className='text'>&#9776; Магазин </div></button>
         </div> 
